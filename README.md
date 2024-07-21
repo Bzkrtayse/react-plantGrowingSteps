f8f66d3cb5afe87e991491dd224e51efd8413410
Ayşe Bozkurt <aysebzkrt47@gmail.com>
2024-07-21 15:26:03 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Plant Growing Steps App
+
+## Overview
+
+This React application provides a step-by-step guide on how to grow a plant. It walks users through three main stages: planting, watering, and caring for the plant. Users can navigate through these steps using "Previous" and "Next" buttons.
+
+## Features
+
+- **Step Navigation:** Users can navigate between steps to view detailed instructions.
+- **Step Indicator:** Visual indicators show the current step in the process.
+- **Interactive Buttons:** Includes buttons to learn more about each step and to navigate through the steps.
+- **Responsive Design:** The app adjusts to different screen sizes.
+
+## Installation
+
+1. Clone the repository:
+
+   `git clone https://github.com/Bzkrtayse/react-plantGrowingSteps.git`
+
+2. Navigate to the project directory:
+
+   `cd react-plantGrowingSteps`
+
+3. Install dependencies:
+
+   `npm install`
+
+## Usage
+
+1. Start the development server:
+
+   `npm start`
+
+2. Open your browser and navigate to `http://localhost:3000`.
---
+++ src/App.js
@@ -0,0 +1,100 @@
+import { useState } from "react";
+
+const messages = [
+  "Choose and Plant 🌱",
+  "Establish a Watering Schedule 💦",
+  "Provide Proper Care 🪄",
+];
+
+export default function App() {
+  return (
+    <div>
+      <Steps />
+    </div>
+  );
+}
+
+function Steps() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) {
+      setStep((s) => s + 1);
+    }
+  }
+
+  return (
+    <div>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      <h1
+        style={{
+          textAlign: "center",
+          color: "#75975e",
+          fontFamily: "Arial, Helvetica, sans-serif",
+        }}
+      >
+        How to Grow Plant
+      </h1>
+
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button
+                bgColor="#e7e7e7"
+                textColor="#333"
+                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
+              >
+                Learn how
+              </Button>
+            </div>
+          </StepMessage>
+
+          <div className="buttons">
+            <Button bgColor="#75975e" textColor="#fff" onClick={handlePrevious}>
+              <span>👈</span> Previous
+            </Button>
+
+            <Button bgColor="#75975e" textColor="#fff" onClick={handleNext}>
+              Next <span>👉</span>
+            </Button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
